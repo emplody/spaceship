11579928f0bdd2a381264f1823ad1a1aed5c68f2
SoYoung210 <ethdud1@gmail.com>
2019-07-16 21:51:24 +0900

    ✨ feat: Change spaceship index

--- utils/spaceship/index.js
+++ utils/spaceship/index.js
@@ -1,5 +1,7 @@
 import paramsToObj from '../spaceship.paramsToObj';
 import utilExample from '../spaceship.utilExample';
-import getPadNumber from '../spaceship.getPadNumber'
+import getPadNumber from '../spaceship.getPadNumber';
+import toTimeString from '../spaceship.toTimeString';
+import uniqueKey from '../spaceship.uniqueKey';
 
-export { paramsToObj, getPadNumber, utilExample };
+export { paramsToObj, getPadNumber, toTimeString, uniqueKey, utilExample };
