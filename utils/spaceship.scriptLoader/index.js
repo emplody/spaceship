bd7afce73653b26e5cb726c285d52dcc4abfe5ea
abelko <esung1129@gmail.com>
2019-07-17 12:55:55 +0900

    :sparkles: feat: add scriptLoader

---
+++ utils/spaceship.scriptLoader/index.js
@@ -0,0 +1,9 @@
+export default scriptLoader = src => {
+    const el = document.createElement('script');
+    el.src = src;
+    document.body.appendChild(el);
+    return new Promise((resolve, reject) => {
+        el.onload = event => resolve(event);
+        el.onerror = () => reject(new Error(`Not loaded script '${src}'`));
+    })
+};
