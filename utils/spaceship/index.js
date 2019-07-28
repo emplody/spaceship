0b691aa8e5363ff0611bfb7fa8192b2920a823f2
seungkyu2 <zx6658@naver.com>
2019-07-28 11:15:48 +0900

    feat: :wrench: editspaceship index.js

--- utils/spaceship/index.js
+++ utils/spaceship/index.js
@@ -1,9 +1,17 @@
-import detectBrowser from './spaceship.detectBrowser';
-import normalizeKeyword from './normalizeKeyword';
+import detectBrowser from '../spaceship.detectBrowser';
+import normalizeKeyword from '../spaceship.normalizeKeyword';
 import paramsToObj from '../spaceship.paramsToObj';
 import objToParams from '../spaceship.objToParams';
 import scriptLoader from '../spaceship.scriptLoader';
 import asyncState from '../spaceship.asyncState';
 import utilExample from '../spaceship.utilExample';
 
-export { detectBrowser, normalizeKeyword, paramsToObj, objToParams, scriptLoader, asyncState, utilExample };
+export {
+  detectBrowser,
+  normalizeKeyword,
+  paramsToObj,
+  objToParams,
+  scriptLoader,
+  asyncState,
+  utilExample
+};
